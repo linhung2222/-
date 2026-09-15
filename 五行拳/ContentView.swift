337f47506a065762924d20cec7d9c74ec564a146
Thomas <linhung0903@gmail.com>
2026-09-15 11:47:07 +0800

    Initial Commit

---
+++ 五行拳/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  五行拳
+//
+//  Created by 115-1student21 on 2026/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
